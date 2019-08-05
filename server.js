d3c7b42ffccf91af9db69394be2c78e19f7ff16f
EdSystango <nitingour99@gmail.com>
2019-08-05 12:09:17 +0530

    final commit

--- server.js
+++ server.js
@@ -10,9 +10,9 @@ app.set('view engine','hbs');
 
 app.use(session({secret:'asdfsd234324',saveUninitialized:true,resave:false}));
 
-
-app.listen(3000,(request,response)=>{
-  console.log("server started:3000");
+const PORT=process.env.PORT || 5000;
+app.listen(PORT,(request,response)=>{
+  console.log("server started:"+PORT);
 });
 
 app.get('/',(request,response)=>{
